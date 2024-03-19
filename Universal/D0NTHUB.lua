a3b5c48d8454b0f48441bbd0a60455b4a3e45640
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-19 16:34:30 +0300

    Update D0NTHUB.lua

--- Universal/D0NTHUB.lua
+++ Universal/D0NTHUB.lua
@@ -1,5 +1,8 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
 
+-- Services:
+local dwPlayers = game:GetService("Players")
+
 library.rank = "developer"
 local wm = library:Watermark("D0NTHUB | alpha build | " .. library:GetUsername() .. " | rank: " .. library.rank)
 
@@ -7,16 +10,9 @@ library.title = "D0NTHUB"
 
 library:Introduction()
 wait(1)
-local Init = library:Init()
+local Init = library:Init(Enum.KeyCode.Delete)
 
 local MainTab = Init:NewTab("Main")
 
 local LocalPlayerSection = MainTab:NewSection("LocalPlayer")
 
-local OtherTab = Init:NewTab("Other")
-
-local MenuSection = OtherTab:NewSection("Menu")
-
-local UnhookButton = OtherTab:NewButton("Unhook", function()
-    library.TabLibrary:Remove()
-end)
