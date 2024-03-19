eb68d7d1dcb442ebab44246888307a23950a5319
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-19 16:38:38 +0300

    Update D0NTHUB.lua

--- Universal/D0NTHUB.lua
+++ Universal/D0NTHUB.lua
@@ -3,6 +3,10 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consi
 -- Services:
 local dwPlayers = game:GetService("Players")
 
+-- Vars:
+local dwLocalPlayer = dwPlayers.LocalPlayer
+local dwHumanoid = dwLocalPlayer.Humanoid
+
 library.rank = "developer"
 local wm = library:Watermark("D0NTHUB | alpha build | " .. library:GetUsername() .. " | rank: " .. library.rank)
 
@@ -16,3 +20,10 @@ local MainTab = Init:NewTab("Main")
 
 local LocalPlayerSection = MainTab:NewSection("LocalPlayer")
 
+local LocalPlayerWalkSpeedSlider = MainTab:NewSlider("Custom WalkSpeed:", "", true, "/", {min = 0, max = 500, default = 16}, function(value)
+    dwHumanoid.WalkSpeed = value
+end)
+
+local LocalPlayerJumpPowerSlider = MainTab:NewSlider("Custom JumpPower:", "", true, "/", {min = 0, max = 500, default = 50}, function(value)
+    dwHumanoid.JumpPower = value
+end)
