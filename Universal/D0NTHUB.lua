1b7da0fd403043ae7c778f78a822b7af393f80b1
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-19 16:39:42 +0300

    Update D0NTHUB.lua

--- Universal/D0NTHUB.lua
+++ Universal/D0NTHUB.lua
@@ -5,7 +5,7 @@ local dwPlayers = game:GetService("Players")
 
 -- Vars:
 local dwLocalPlayer = dwPlayers.LocalPlayer
-local dwHumanoid = dwLocalPlayer.Humanoid
+local dwHumanoid = dwLocalPlayer.Character.Humanoid
 
 library.rank = "developer"
 local wm = library:Watermark("D0NTHUB | alpha build | " .. library:GetUsername() .. " | rank: " .. library.rank)
