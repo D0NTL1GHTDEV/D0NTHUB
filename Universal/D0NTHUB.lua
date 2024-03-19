f3f4f3c3153a2d311c14f4b25f49cb060b7605d8
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-19 16:29:22 +0300

    Update D0NTHUB.lua

--- Universal/D0NTHUB.lua
+++ Universal/D0NTHUB.lua
@@ -18,5 +18,5 @@ local OtherTab = Init:NewTab("Other")
 local MenuSection = OtherTab:NewSection("Menu")
 
 local UnhookButton = OtherTab:NewButton("Unhook", function()
-    library:Remove()
+    library.screen:Destroy()
 end)
