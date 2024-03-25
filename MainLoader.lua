5cada408289164551b429de584d097456d4427e6
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-25 21:49:35 +0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,6 +1,4 @@
-local pid = game.PlaceId
-
-if pid == 6576912941 then
+if game.PlaceId == 6576912941 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/D0NTL1GHTDEV/D0NTHUB/main/ACG/D0NTHUB.lua"), true)()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/D0NTL1GHTDEV/D0NTHUB/main/Universal/D0NTHUB.lua"), true)()
