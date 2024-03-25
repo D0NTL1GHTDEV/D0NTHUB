56c93922f684b931b782a53d264722dbbd90a6d7
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-25 21:50:09 +0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6576912941 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/D0NTL1GHTDEV/D0NTHUB/main/ACG/D0NTHUB.lua"), true)()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/D0NTL1GHTDEV/D0NTHUB/main/ACG/D0NTHUB.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/D0NTL1GHTDEV/D0NTHUB/main/Universal/D0NTHUB.lua"), true)()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/D0NTL1GHTDEV/D0NTHUB/main/Universal/D0NTHUB.lua"))()
 end
