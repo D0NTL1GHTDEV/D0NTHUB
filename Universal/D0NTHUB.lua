d5e6de93e57e9e34e4b10dcc952f9e2687396bf1
D0NTL1GHT <notlight.artem@gmail.com>
2024-04-02 17:47:41 +0300

    Update D0NTHUB.lua

--- Universal/D0NTHUB.lua
+++ Universal/D0NTHUB.lua
@@ -18,7 +18,7 @@ local Window = Rayfield:CreateWindow({
     KeySettings = {
        Title = "D0NT HUB",
        Subtitle = "Key System",
-       Note = "Join the discord (discord.gg/sirius)",
+       Note = "Join the discord (dsc.gg/d0nthub)",
        FileName = "D0NTHUBKey",
        SaveKey = true,
        GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
@@ -27,3 +27,5 @@ local Window = Rayfield:CreateWindow({
 })
 
 local MainTab = Window:CreateTab("Main", 4483362458)
+
+local AimbotSection = Tab:CreateSection("Aimbot")
